ffd512b4b87fda602f969764480479e1d76189d5
Kumbhar <pooja-prakash.kumbhar@capgemini.com>
2019-04-09 09:08:12 +0530

    yes done

---
+++ bankapp_jdbc_config/src/BankAccountNotFoundException.java
@@ -0,0 +1,15 @@
+package com.capgemini.bankapp.exception;
+
+public class BankAccountNotFoundException extends Exception {
+
+	public AccountNotFoundException() {
+		super();
+
+	}
+
+	public AccountNotFoundException(String message) {
+		super(message);
+
+	}
+
+}
